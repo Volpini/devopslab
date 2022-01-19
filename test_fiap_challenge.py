f09b485bf1ec439e76b4dfdf3b014b8c0af6cf52
Bruno Frigo Volpini <bruno-volpini@hotmail.com>
2022-01-19 19:13:53 -0300

    Desafio Cobertura de Código - Adicionando testes aos novos métodos

---
+++ test_fiap_challenge.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from app import app
+import unittest
+
+class TestFiapChallenge(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/fiap/challenge')
+
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "This increments the number of lines of code to allow SonarQube to actually scan the entire code")
