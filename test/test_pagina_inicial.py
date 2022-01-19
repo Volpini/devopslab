1c1ee1a33a56e511f2740642cf056932e8bc9606
Bruno Frigo Volpini <bruno-volpini@hotmail.com>
2022-01-19 19:18:06 -0300

    Desafio Cobertura de Codigo - Configurando todos os testes para rodar no pipeline

--- test/test_pagina_inicial.py
+++ test/test_pagina_inicial.py
@@ -2,7 +2,7 @@
 from app import app
 import unittest
 
-class Test(unittest.TestCase):
+class TestPaginaInicial(unittest.TestCase):
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
