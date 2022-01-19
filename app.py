ef2cbc5de781e0de13faceffc6754aa7882bfdec
Bruno Frigo Volpini <bruno-volpini@hotmail.com>
2022-01-19 18:40:18 -0300

    Fix app return page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Hello World - Bruno Volpini"
 
 if __name__ == '__main__':
     app.run()
