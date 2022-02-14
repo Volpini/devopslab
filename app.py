5a8a8facd6f5a72ec0ba4b0261169cae44d0e394
Bruno Volpini <bruno-volpini@hotmail.com>
2022-02-14 20:56:40 -0300

    Prepare Dockerfile and app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
@@ -50,4 +50,6 @@ def tura():
     return "cober"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
+
